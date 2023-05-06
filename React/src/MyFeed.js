3f6861ec025181cb3893326ed8711a6039f580b6
mynyamray <59934771+mynyamray@users.noreply.github.com>
2023-05-06 02:36:45 -0700

    Update MyFeed.js

--- React/src/MyFeed.js
+++ React/src/MyFeed.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './MyFeed.css';
 function MyFeed() {
-  const [postContent, setPostContent] = useState("");
+  const [username, setUsername] = useState("");
+  const [postContent, setPostContent] = useState("Post Goes Here");
   const [likeCount, setLikeCount] = useState(0);
   const [dislikeCount, setDislikeCount] = useState(0);
   const fetchPostData = () => {
     //change the api address to connect
-    fetch("/api")
+    fetch("/MyFeed")
       .then((response) => response.json())
       .then((data) => {
         //change dictionary name to match flask
@@ -20,7 +21,7 @@ function MyFeed() {
   }, []);
   const handleLikeClick = () => {
     //change dictionary name to match flask
-    fetch("/api", {method: "POST"})
+    fetch("/like", {method: "PUT"})
       .then((response) => response.json())
       .then((data) => {
         setLikeCount(data.likeCount);
@@ -29,21 +30,30 @@ function MyFeed() {
   };
   const handleDislikeClick = () => {
     //change dictionary name to match flask
-    fetch("/api", {method: "POST"})
+    fetch("/dislike", {method: "PUT"})
       .then((response) => response.json())
       .then((data) => {
         setLikeCount(data.setDislikeCount);
         fetchPostData();
     });
   };
+  const handleFollow = () => {
+    //change dictionary name to match flask
+    fetch("/api", {method: "PUT"})
+      .then((response) => response.json())
+      .then((data) => {
+        setUsername(data.setUsername);
+        fetchPostData();
+    });
+  };
   return (
     //change dictionary name to match flask
     <div className='MyFeed'>
-      <button className = "like" onClick={handleLikeClick}></button>
-      <h4>Like ({likeCount})</h4>
+      <button className = "like" onClick={handleLikeClick}>{likeCount}</button>
+      <h3>{username}</h3>
+      <button className= "follow" onclick={handleFollow}>Follow</button>
       <h2 className='feed'>{postContent}</h2>
-      <button className = "dislike" onClick={handleDislikeClick}></button>
-      <h4>Dislike ({dislikeCount})</h4>
+      <button className = "dislike" onClick={handleDislikeClick}>{dislikeCount}</button>
     </div>
   );
 }
